0ef4bb83f506e4fce5399644c76b58e33814b371
Fausto Ortega <fiortegas@utn.edu.ec>
2020-11-03 20:05:25 -0500

    Actualizacion de Codigo

--- One_Shot/one_shot_init.m
+++ One_Shot/one_shot_init.m
@@ -7,11 +7,16 @@ starttime = 0.0;       % control task start time
 period = 0.05;          % control task period
 data.h = 0.05;          % periodo
 %% Inicializacion Variables de Datos One-Shoot
-data.u=0;              % señal de control
-data.xk = 0;           % Señal de referencia       
-data.y_an = 0;         % Salida Anterior
-data.u_an = 0;         % señal de control anterior
-data.tk_tau=0;         % tiempo entre muestras
+data.u = 0;              % señal de control
+data.xk = 0;                 
+data.y_an = 0;           % Salida Anterior
+data.u_an = 0;           % señal de control anterior
+data.tk_tau = 0;        
+%% Matrices de la Dinamica del Sistema
+data.A = [0 1 ; -918.27 -90.90];   % matris de transicion de estado
+data.B = [0;918.27];               % matris de trancision de control
+data.C = [1 0];                    % matris de salida
+data.D = 0;
 
 %% creacion de tarea periodica
 ttCreatePeriodicTask('ctrl_task', starttime, period, 'one_shot_code', data)
--- One_Shot/task_control.m
+++ One_Shot/task_control.m
@@ -1,19 +1,15 @@
-function [ u] = task_control( xk, tau_k, u_an,y_an )
+function [ u] = task_control( data, tau_k )
+%% Espacio de Estados
+sys = ss(data.A,data.B,data.C,data.D);
 
-%% Matrices de la Dinamica del Sistema
-A=[0 1 ; -918.27 -90.90];   % matris de transicion de estado
-B=[0;918.27];               % matris de trancision de control
-C=[1 0];                    % matris de salida
-D = 0;
 %% Ubicacion de los polos
-p = [-42+42*i -42-42*i]; 
- 
-L = acker(A,B,p);
+p = [-42+42*i -42-42*i];  
+L = acker(sys.A,sys.B,p);
 
 %% Tarea de Control
-xk_vec = [xk;u_an];
-[fi, T ]= c2d(A,B,tau_k);
-X_k_tao = (fi * xk_vec) + ( T * y_an);
+xk_vec = [data.xk;data.u_an];
+[fi, T ] = c2d(sys.A, sys.B, tau_k);
+X_k_tao = (fi * xk_vec) + ( T * data.y_an);
 u = L * X_k_tao;           % Calculo de la Señal de salida
 
 end
